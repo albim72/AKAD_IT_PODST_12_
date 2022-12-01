c7477d6ccbfbe1b4ada3d0db668e580d6a4c4471
albim72 <marcin.albiniak72@gmail.com>
2022-12-01 10:21:33 +0100

    Create podstawy.py

---
+++ DZIEN_1/podstawy.py
@@ -0,0 +1,17 @@
+print("pierwsza linia programu....")
+a = 67
+print(a)
+print(type(a))
+
+a = "czerwony"
+print(a)
+print(type(a))
+
+liczba:float
+liczba = 8.89
+print(liczba)
+print(type(liczba))
+
+liczba = True
+print(liczba)
+print(type(liczba))
