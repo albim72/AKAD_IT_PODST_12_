e1b340d825c2c843be6664b8288d5faba3b1df10
albim72 <marcin.albiniak72@gmail.com>
2022-12-01 14:39:42 +0100

    Create moje_funkcje.py

---
+++ DZIEN_1/moje_funkcje.py
@@ -0,0 +1,21 @@
+#przykład nr 1
+def witaj():
+    print("witaj użytkowniku")
+    print("opłać abonament")
+    print("zaloguj się")
+
+witaj()
+witaj()
+
+# for _ in range(21):
+#     witaj()
+
+#przykład nr 2
+def obywatel(kraj="Polska"):
+    print("pochodzę z kraju:",kraj)
+
+obywatel("Turcja")
+obywatel("Armenia")
+obywatel("Włochy")
+obywatel("Hiszpania")
+obywatel()
