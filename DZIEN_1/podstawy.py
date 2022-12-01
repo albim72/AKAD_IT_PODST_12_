9998e90108a272358accd826e6d995bb9f6f9a70
albim72 <marcin.albiniak72@gmail.com>
2022-12-01 10:33:20 +0100

    Update podstawy.py

--- DZIEN_1/podstawy.py
+++ DZIEN_1/podstawy.py
@@ -15,3 +15,28 @@ print(type(liczba))
 liczba = True
 print(liczba)
 print(type(liczba))
+
+pora = "pora roku: "
+pora1 = "jesień"
+pora2 = "zima"
+rok = 2022
+
+#komentarz -> łączenie ciąg string (tekstu) -> operator +
+print(pora+pora1+", "+pora+pora2+", rok: "+ str(rok))
+
+"""
+komentarz dokumentacyjny, wieloliniowy
+tylko pierwszy komentarz w pliku pythona jest dokumentacyjny
+linia3
+linia4
+.....
+
+"""
+'''
+ten komentarz nie jest dokumentacyjny
+'''
+
+#wyświetlanie tekstu za poocą kolekcji
+print(pora,pora1,",",pora,pora2,"rok:",rok,sep=" -- ")
+
+
