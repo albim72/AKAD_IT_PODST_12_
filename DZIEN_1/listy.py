7a992423171c250e12a4903c9a2487209ccc5a4a
albim72 <marcin.albiniak72@gmail.com>
2022-12-01 11:52:55 +0100

    Update listy.py

--- DZIEN_1/listy.py
+++ DZIEN_1/listy.py
@@ -16,3 +16,27 @@ kraje.sort(reverse=True)
 print(kraje)
 
 liczby = [2,56,78,90,-32,0,1112,0,12,9,-3,134,67,-8,0,12,12]
+
+liczby.sort(reverse=True)
+print(liczby)
+
+print(liczby[1:6])
+liczby.remove(1112)
+print(liczby)
+
+del liczby[2]
+print(liczby)
+
+print(liczby.count(0))
+
+inc = liczby.index(-3)
+del liczby[inc]
+print(liczby)
+
+rasa = ["buldog angelski","rotwalier","labrador"]
+rasa_cena = [7500,6500,4500]
+sklepzoo = [[rasa,"kot","papuga","złota rybka","mysz"],[rasa_cena,2000,8000,40,45]]
+print(sklepzoo[0])
+print(sklepzoo[0][0])
+print(sklepzoo[0][0][0],"-",sklepzoo[1][0][0],"zł")
+print(sklepzoo[0][1],"-",sklepzoo[1][1],"zł")
