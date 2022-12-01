e2da5384b1912053805b063eda6876f02b945bc0
albim72 <marcin.albiniak72@gmail.com>
2022-12-01 11:36:53 +0100

    Create listy.py

---
+++ DZIEN_1/listy.py
@@ -0,0 +1,18 @@
+kraje = ["Polska","Kanada","Turcja","Hiszpania","Laos","Japonia"]
+print(kraje)
+print(type(kraje))
+print(kraje[3])
+print(kraje[2:5])
+kraje.append("Urugwaj")
+print(kraje)
+kraje.insert(2,"Egipt")
+print(kraje)
+
+kraje.sort()
+print(kraje)
+kraje.reverse()
+print(kraje)
+kraje.sort(reverse=True)
+print(kraje)
+
+liczby = [2,56,78,90,-32,0,1112,0,12,9,-3,134,67,-8,0,12,12]
