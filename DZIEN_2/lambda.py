574118e77f8bbac2dce5186154b0f3ddb597b3c1
albim72 <marcin.albiniak72@gmail.com>
2022-12-02 08:56:40 +0100

    Update lambda.py

--- DZIEN_2/lambda.py
+++ DZIEN_2/lambda.py
@@ -18,3 +18,20 @@ print(d(6))
 print(multi(4)(3))
 
 nb = [3,6,0,-2,16,17,23,24,90,99,101,120,-9,0,12,8]
+
+#stwórz nową listę parzyste, i umieść w niej tylko wartości parzyste z listy nb
+#filter(funkcja,dane) > funkcja ma zawierać warunek filtrowania, dane -> dane do filtrowania
+parzyste = list(filter(lambda x:x%2==0,nb))
+print(parzyste)
+
+def war(x):
+    return x%2!=0
+
+parzyste_ext = list(filter(war,nb))
+print(parzyste_ext)
+
+#stwórz nową listę cube, i umieść w niej wszystkie  z listy nb podniesione do sześcianu
+#map(funkcja,dane) > funkcja ma zawierać transformację, dane -> dane do mapowania
+
+cube = list(map(lambda x:x**3,nb))
+print(cube)
