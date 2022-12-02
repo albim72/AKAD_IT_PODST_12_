5544fbac7c3df9b8e8684f36cad9f4eb7b5f78d3
albim72 <marcin.albiniak72@gmail.com>
2022-12-02 09:17:25 +0100

    Update wycieczka.py

--- DZIEN_2/wycieczka.py
+++ DZIEN_2/wycieczka.py
@@ -1,2 +1,7 @@
 #funkcja kwota(transport,nocleg_wyzyw,wycieczki,ubezp,inne,rabat) -> wypisz wynik
 #rabatuj tylko transport i nocleg_wyzyw
+def kwota(t,nw,w,u,i,rab=0):
+    return (nw+t)*(1-rab/100)+w+u+i
+
+print(kwota(100,100,50,50,50),"zł")
+print(kwota(100,100,50,50,50,25),"zł")
